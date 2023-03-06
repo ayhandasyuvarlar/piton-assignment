6e1350b66688df0f5d4004f605d9220c53c5b11e
Ayhan Dasyuvarlar <ayhandasyuvarlar@gmail.com>
2023-03-06 14:50:49 +0300

    category

--- app/[...category]/page.js
+++ app/[...category]/page.js
@@ -1,11 +1,28 @@
-import { notFound } from "next/navigation";
+"use client";
+import { fetchCategoriesSearch } from "@/reducer/categorySearchSlice";
+import { fetchProducts } from "@/reducer/productsSlice";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-const Category = ({ params, searchParams }) => {
-  // const category = "metafizik";
-  // if (searchParams.product !== category) {
-  //   notFound();
-  // }
-  return <div>hell word</div>;
+const Category = ({ searchParams }) => {
+  const categoryId = searchParams.category;
+  const dispatch = useDispatch();
+  const categoryName = useSelector((state) => state.category.category);
+  useEffect(() => {
+    dispatch(fetchCategoriesSearch(categoryId));
+  }, [dispatch, categoryId]);
+
+  return (
+    <div>
+      {categoryName && categoryName.length > 0 && (
+        <>
+          {categoryName.map((item) => (
+            <div key={item.id}>{item.name}</div>
+          ))}
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Category;
