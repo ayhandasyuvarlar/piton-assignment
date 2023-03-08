045c7e735b01f65b4cb2f919c5fb5f787fca0896
ayhan dasyuvarlar <ayhandasyuvarlar3@gmail.com>
2023-03-08 11:33:01 +0300

    auth reducer

---
+++ reducer/authSlice.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+
+const { createSlice, createAsyncThunk } = require("@reduxjs/toolkit");
+
+const initialState = {
+  msg: "",
+  user: "",
+  token: "",
+  loading: false,
+  error: "",
+};
+
+export const signUpUser = createAsyncThunk("signUp", async (data) => {
+  const { email, password, name } = data;
+  const res = await axios.post(
+    "https://assign-api.piton.com.tr/api/rest/register",
+    {
+      email,
+      name,
+      password,
+    }
+  );
+  return await res.data;
+});
+
+const authSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(signUpUser.pending, (state, action) => {
+      state.loading = true;
+    });
+    builder.addCase(signUpUser.fulfilled, (state, action) => {
+      // state.loading = false;
+      // if (action.payload.error) {
+      //   state.error = action.payload.error;
+      // } else {
+      //   state.user = action.payload;
+      // }
+      state.loading = false;
+      state.msg = action.payload;
+    });
+    builder.addCase(signUpUser.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.error;
+    });
+  },
+});
+
+export default authSlice.reducer;
